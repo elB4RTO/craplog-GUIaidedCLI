8d5ad3248f11d02da79ee270dcd35c1c445eaf20
elBarto-tools <81904638+elBarto-tools@users.noreply.github.com>
2021-08-16 16:49:11 +0200

    Update help.sh

--- craplog/aux/help.sh
+++ craplog/aux/help.sh
@@ -3,7 +3,7 @@
 # GETTING PATH
 crapdir="$(dirname $(realpath $0))"
 # SHOWING HELP MESSAGE
-cat "$crapdir"/elbarto "$crapdir"/craplogo "$crapdir"/help
+cat "$crapdir"/elbarto.txt "$crapdir"/craplogo.txt "$crapdir"/help.txt
 # CLOSE WHEN READY
 printf "\n$(tput setaf 8)Press any key to continue ...$(tput sgr0)"
 read closing
